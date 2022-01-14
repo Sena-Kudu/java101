604c993ba47edfa1a09d6eae9d741c647c89279e
Sena Kudu <60321214+Sena-Kudu@users.noreply.github.com>
2022-01-14 18:05:52 +0300

    Add files via upload

---
+++ mukemmelSayii.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;  
+
+public class MyClass {
+
+    
+    public static void main(String args[]) {
+    
+      Scanner scanner = new Scanner(System.in);
+      int sayi = scanner.nextInt();
+      int toplam = 0;
+ 
+     
+      for(int i=1; i<sayi; i++){
+          
+        if(sayi%i == 0)
+            toplam+=i;
+      
+      }
+      
+      if(sayi == toplam)
+        System.out.println(sayi+" Mükemmel sayıdır");
+      else
+         System.out.println(sayi+" Mükemmel sayı değildir");
+     
+    }
+}
